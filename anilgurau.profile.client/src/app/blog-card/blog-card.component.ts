91241d4d98a0b62f0aff2852c031e3e4a18a0403
gurau-anil <gurau.anil@gmail.com>
2025-05-08 15:18:30 -0700

    changes in card component

--- anilgurau.profile.client/src/app/blog-card/blog-card.component.ts
+++ anilgurau.profile.client/src/app/blog-card/blog-card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { TruncatePipe } from '../pipes/truncate.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog-card',
   standalone: true,
+  imports:[TruncatePipe, CommonModule],
   templateUrl: './blog-card.component.html',
   styleUrl: './blog-card.component.scss'
 })
 export class BlogCardComponent {
-
+  @Input() post!: Post;
 }
