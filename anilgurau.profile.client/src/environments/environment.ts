703ae9ec14540e7be0dfc12eaa3d2e8d53e2a07f
gurau-anil <gurau.anil@gmail.com>
2025-05-11 14:33:43 -0700

    adding environments

---
+++ anilgurau.profile.client/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+
+    hashnodeUrl: 'https://anilgurau.hashnode.dev',
+    linkedInUrl: "https://www.linkedin.com/in/anil-gurau",
+    upworkUrl: "https://www.upwork.com/freelancers/~01f1f6114c21122cb4",
+    githubUrl: "https://github.com/gurau-anil",
+    emailAddress: "contact@anilgurau.com",
+    hashnodeApiBaseUrl: "https://gql.hashnode.com",
+    hashnodeBlogHost: "anilgurau.hashnode.dev",
+};
